8c1fb753f934dd4e1f1c51acdcb9056349c66e11
alexnicolini <alexnicolini@gmail.com>
2015-08-24 15:46:39 -0300

    Move Tweet Insertion to Server

--- client/js/tweetBox.js
+++ client/js/tweetBox.js
@@ -34,8 +34,6 @@ Template.tweetBox.events({
     var tweet = $('#tweetText').val();
     $('#tweetText').val('');
     Session.set('numChars', 0);
-    if (Meteor.user()) {
-      Tweets.insert({ message: tweet, user: Meteor.user().username });
-    }
+    Meteor.call('insertTweet', tweet);
   }
 });
