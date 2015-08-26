63eef0e676c6e8e9ab8e6bb3f34c93bf6e667a99
alexnicolini <alexnicolini@gmail.com>
2015-08-25 21:30:09 -0300

    Use Pub/Sub for Follow Recommendations

--- server/js/followUsers.js
+++ server/js/followUsers.js
@@ -14,22 +14,5 @@ Meteor.methods({
       follower: Meteor.user().username,
       following: username
     });
-  },
-
-  'recommendedUsers': function() {
-    if (Meteor.user()) {
-      var currentFollowings = UserUtils.findFollowings(Meteor.user().username);
-
-      var recUsers = Meteor.users.find({
-        username: {
-          $nin: currentFollowings
-        }
-      }, {
-        fields: { 'username': 1 },
-        limit: 5
-      }).fetch();
-
-      return recUsers;
-    }
   }
 });
